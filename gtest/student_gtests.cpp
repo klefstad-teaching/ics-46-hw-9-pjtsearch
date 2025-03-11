e1e5679f05d49685f90772c64e4fd7bac563c00e
Peter PJT <pjtsearch@protonmail.com>
2025-03-11 12:04:51 -0700

    feat(edit_distance_within): Implement and test edit_distance_within

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,14 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+TEST(edit_distance_within, full_test)
+{
+  EXPECT_TRUE(edit_distance_within("a", "ab", 1));
+  EXPECT_TRUE(edit_distance_within("aa", "ab", 1));
+  EXPECT_FALSE(edit_distance_within("sa", "ab", 1));
+  EXPECT_FALSE(edit_distance_within("nn", "ab", 1));
+  EXPECT_FALSE(edit_distance_within("aaa", "ab", 1));
+  EXPECT_TRUE(edit_distance_within("aaa", "ab", 2));
+  EXPECT_TRUE(edit_distance_within("car", "hats", 3));
+  EXPECT_FALSE(edit_distance_within("car", "hats", 2));
 }
---
+++ src/ladder.cpp
@@ -0,0 +1,15 @@
+#include "ladder.h"
+
+bool edit_distance_within(const string &str1, const string &str2, int d)
+{
+    // subtract from d each time, and then follow Levenshtein
+    if (str1.size() == 0)
+        return int(str2.size()) <= d;
+    if (str2.size() == 0)
+        return int(str1.size()) <= d;
+    if (str1[0] == str2[0])
+        return edit_distance_within(str1.substr(1), str2.substr(1), d);
+    return edit_distance_within(str1.substr(1), str2, d - 1) ||
+           edit_distance_within(str1, str2.substr(1), d - 1) ||
+           edit_distance_within(str1.substr(1), str2.substr(1), d - 1);
+}
